aec78a425c67536169f506984e0132fd2f4f2272
Grégoire Lemelin <202492084@cegepsherbrooke.qc.ca>
2025-02-10 14:53:35 -0500

    Create lab4.py

---
+++ lab4.py
@@ -0,0 +1,11 @@
+#continuer de travailler sur votre code
+#possible de changer
+# deux for et deux while
+# trois input de l'utilisateur
+# des print, genre beaucoup
+# trois bloc de if, nombre de "if" 
+
+#break
+#continue
+# else (boucle)
+# si on viens au lab en sachant ce que le mot "pass" veut dire
